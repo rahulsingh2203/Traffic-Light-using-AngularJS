5bb493ade7abb3ac5c65aceb06df92684c60fd0e
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-01-21 18:43:38 +0530

    working on btn functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TrafficLight';
+  color = 'red';
+  red(){
+    //let color = 'red';
+    console.log("Red button is Working!");
+  }
+  yellow(){
+    //let color = 'yellow';
+    console.log("Yellow button is Working!");
+  }
+  green(){
+    //let color = 'green';
+    console.log("Green button is Working!");
+  }
 }
