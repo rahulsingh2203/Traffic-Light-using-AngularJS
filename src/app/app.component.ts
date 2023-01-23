3c4d2673a764dca6c22f43ff0b56eea29e47771f
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-01-23 15:28:36 +0530

    task completed!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TrafficLight';
-  color = 'red';
-  red(){
-    //let color = 'red';
+  color1 = 'black';
+  color2 = 'black';
+  color3 = 'black';
+  red() {
+    this.color1 = 'red';
+    this.color2 = 'black';
+    this.color3 = 'black';
     console.log("Red button is Working!");
   }
-  yellow(){
-    //let color = 'yellow';
+  yellow() {
+    this.color1 = 'black';
+    this.color2 = 'yellow';
+    this.color3 = 'black';
     console.log("Yellow button is Working!");
   }
-  green(){
-    //let color = 'green';
+  green() {
+    this.color1 = 'black';
+    this.color2 = 'black';
+    this.color3 = 'green';
     console.log("Green button is Working!");
   }
 }
